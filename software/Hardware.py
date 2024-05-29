a77490e9096071f1520e6f66b0f55af058cda50e
Dominator307 <thummererdo@gmail.com>
2024-05-29 20:37:53 +0200

    MPU + Hardware

--- software/Hardware.py
+++ software/Hardware.py
@@ -1,6 +1,6 @@
 import os, time, Helper as h 
 import RPi.GPIO as GPIO
-import smbus
+import smbus, MPU6000 as MPU
 from Helper import pprint as print
 
 
@@ -50,21 +50,17 @@ def servo1(dutycycle:float):
     GPIO.cleanup()
     return
 
-def servo2():
+def servo2(dutycycle:float):
     GPIO.setmode(GPIO.BCM)
-    servo2PIN = 12
+    servo2PIN = h.pins['Servo2']
     GPIO.setup(servo2PIN, GPIO.OUT)
     p2 = GPIO.PWM(servo2PIN, 50)
-    p2.start(7.5)
-    p2.ChangeDutyCycle(7.5)
-    time.sleep(1)
-    p2.ChangeDutyCycle(12.5)
-    time.sleep(1)
+    p2.start(dutycycle)
     p2.stop()
     GPIO.cleanup()
 
 def watersensor():
-    waterpin = 19
+    waterpin = h.pins['watersensor']
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(waterpin, GPIO.IN)
     try:
@@ -79,46 +75,46 @@ def watersensor():
         GPIO.cleanup()
 def pushbutton1():
     GPIO.setmode(GPIO.BCM)
-    pushbutton1 = 5
+    pushbutton1 = h.pins['pushbutton1']
     GPIO.setup(pushbutton1, GPIO.IN)
     GPIO.cleanup()
 
 def pushbutton2():
     GPIO.setmode(GPIO.BCM)
-    pushbutton2 = 6
+    pushbutton2 = h.pins['pushbutton2']
     GPIO.setup(pushbutton2, GPIO.IN)
     GPIO.cleanup()
 
 def LEDR():
     GPIO.setmode(GPIO.BCM)
-    ledr = 25
+    ledr = h.pins['ledr']
     GPIO.setup(ledr, GPIO.OUT)
     GPIO.output(ledr, True)
     GPIO.cleanup()
 
 def LEDG():
     GPIO.setmode(GPIO.BCM)
-    ledg = 24
+    ledg = h.pins['ledg']
     GPIO.setup(ledg, GPIO.OUT)
     GPIO.output(ledg, True)
     GPIO.cleanup()
 
 def LEDB():
     GPIO.setmode(GPIO.BCM)
-    ledb = 23
+    ledb = h.pins['ledb']
     GPIO.setup(ledb, GPIO.OUT)
     GPIO.output(ledb, True)
     GPIO.cleanup()
 
 def SDA():
     GPIO.setmode(GPIO.BCM)
-    sda = 2
+    sda = h.pins['sda']
     GPIO.setup(sda, GPIO.OUT)
     GPIO.cleanup()
 
 def SCL():
     GPIO.setmode(GPIO.BCM)
-    scl = 3
+    scl = h.pins['scl']
     GPIO.setup(scl, GPIO.OUT)
     GPIO.cleanup()
 
@@ -131,47 +127,7 @@ def get_angle():
     """
     return None
 
-# Implementierung des Gyroskops
-    # GET I2C bus
-    bus = smbus.SMBus(1)
-    bus.write_byte_data(0x68, 0x1B, 0x18)
-    bus.write_byte_data(0x68, 0x1C, 0x18)
-    bus.write_byte_data(0x68, 0x6B, 0x01)
-    time.sleep(0.8)
-    data = bus.read_i2c_block_data(0x68, 0x3B, 6)
 
-    xAccl = data[0] * 256 +data[1]
-    if xAccl > 32767 :
-            xAccl -= 65536
-
-    yAccl = data[2] * 256 + data[3]
-    if yAccl > 32767 :
-        yAccl -= 65536       
-
-    zAccl = data[4] * 256 + data[5]
-    if zAccl > 32767 :
-        zAccl -= 65536
-        
-    data = bus.read_i2c_block_data(0x68, 0x43, 6)
-
-    xGyro = data[0] * 256 + data[1]
-    if xGyro > 32767 :
-        xGyro -= 65536
-
-    yGyro = data[2] * 256 + data[3]
-    if yGyro > 32767 :
-        yGyro -= 65536
-
-    zGyro = data[4] * 256 + data[5]
-    if zGyro > 32767 :
-        zGyro -= 65536
-
-    print "Acceleration in X-Axis : %d" %xAccl
-    print "Acceleration in Y-Axis : %d" %yAccl
-    print "Acceleration in Z-Axis : %d" %zAccl
-    print "X-Axis of Rotation : %d" %xGyro
-    print "Y-Axis of Rotation : %d" %yGyro
-    print "Z-Axis of Rotation : %d" %zGyro
 
 if __name__ == "__main__": 
     hw_init()
